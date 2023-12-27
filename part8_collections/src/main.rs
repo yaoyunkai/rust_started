7897cfcca267a152a883695aab80136b939504aa
libyao <1052433260@qq.com>
2023-12-27 17:40:40 +0800

    rust collections

--- part8_collections/src/main.rs
+++ part8_collections/src/main.rs
@@ -13,15 +13,10 @@ mod vector_usage;
 mod string_usage;
 
 
-mod redis_usage;
-
 fn main() {
     // vector_usage::run_vector();    
     // vector_usage::run_vector_3();
     // vector_usage::use_vector_with_enum();
 
     string_usage::run_with_string();
-
-    // let ret = redis_usage::fetch_an_integer().expect("get error");
-    // println!("the result from redis is {ret}");
 }
