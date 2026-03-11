ae2176623f6b458b9932964d65d7e6bbb47dd263
yunk <1052433260@qq.com>
2026-03-11 23:04:39 +0800

    remove unused packages

--- part10_generics/src/main.rs
+++ part10_generics/src/main.rs
@@ -1,28 +1,13 @@
 // use crate::lifetimes::test_lifetime;
 
 mod generic_start;
-mod trait_usage;
-mod hash_usage;
-mod struct_trait_usage;
 mod lifetimes;
+mod struct_trait_usage;
+mod trait_usage;
 
 fn main() {
-    // generic_start::run_1();
-    // generic_start::run_with_struct();
-
-    // trait_usage::run_trait();
-    // hash_usage::run_hash();
-
-    // trait_usage::run_trait();
-    // struct_trait_usage::run_1();
-
-    // lifetimes::test_lifetime();
-    // lifetimes::test_lifetime2();
-
-
     let string1 = String::from("abcd");
     let string2 = "xyz";
-
     let result = lifetimes::longest(string1.as_str(), string2);
     println!("The longest string is {}", result);
 }
