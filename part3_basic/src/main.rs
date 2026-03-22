7b48e79ce63648fe924ea2a37e4dc0c33c3495e5
yunk <1052433260@qq.com>
2026-03-22 22:04:48 +0800

    update

--- part3_basic/src/main.rs
+++ part3_basic/src/main.rs
@@ -1,3 +1,6 @@
+use regex_syntax::hir::Hir;
+use regex_syntax::parse;
+
 mod control_flow;
 mod data_types;
 mod double_linked_list;
@@ -7,5 +10,14 @@ mod variables;
 
 fn main() {
     // variables::runner();
-    double_linked_list::foo()
+    double_linked_list::foo();
+
+    let hir = parse("a|b").unwrap();
+    assert_eq!(
+        hir,
+        Hir::alternation(vec![
+            Hir::literal("a".as_bytes()),
+            Hir::literal("b".as_bytes()),
+        ])
+    );
 }
